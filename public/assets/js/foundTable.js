a7c5efddbe499b6034b68770029dc8970bad3923
Neha Pathak <neha.pathak@hotmail.com>
2018-09-15 13:19:36 -0400

    updated lost table put request

--- public/assets/js/foundTable.js
+++ public/assets/js/foundTable.js
@@ -1,10 +1,40 @@
 // The code for populating the list of found items at the bottom of the found.html
-var email = $('#exampleFormControlInput1').val();
-var phone = $('#exampleFormControlInput2').val();
-var category = $('#category').val();
-var subcategory = $('.subcategory').val();
-var size = $('.size').val();
-var color = $('.color').val();
-var location = $('.location').val();
-var description = $('').val();
-var claimed = $('').val();
+$(document).ready(function () {
+
+    // items to be added into the table
+    var email = $('#exampleFormControlInput1').val();
+    var phone = $('#exampleFormControlInput2').val();
+    var category = $('#category').val();
+    var subcategory = $('.subcategory').val();
+    var size = $('.size').val();
+    var color = $('.color').val();
+    var location = $('.location').val();
+    var description = $('.description').val();
+    var claimed = false;
+
+    $(document).on('submit', '#itemsEntry', submitLostItem);
+
+    function submitLostItem(event) {
+        event.preventDefault();
+        // Don't do anything if the name fields hasn't been filled out
+        if (!category) {
+            return;
+        }
+        // Calling the upsertAuthor function and passing in the value of the name input
+        addNewItem({
+            email: email,
+            phone: phone,
+            category: category,
+            subcategory: subcategory,
+            size: size,
+            color: color,
+            location: location,
+            description: description,
+            claimed: claimed
+        });
+    }
+
+    function addNewItem(data) {
+        $.post("/api/lost", lost)
+      }
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
 // ******************************************************************************
-// *** Dependenciegs
+// *** Dependencies
 // =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -40,7 +40,7 @@ app.use(apiroutes);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // db.sequelize.sync({ force: true }).then(function() {
-  db.sequelize.sync().then(function() {
+  db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
   });
