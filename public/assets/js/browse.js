2dc08190895dd8734eff4ee209a83465888fbe5d
Durian <karen.kua7@gmail.com>
2018-09-16 14:41:58 -0400

    Finished reconstructing querying lost table code to avoid repeats.

--- public/assets/js/browse.js
+++ public/assets/js/browse.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
 
-
-
-var searchData;
-
 $('#searchBtn').on('click', function() {
     event.preventDefault();
-    // window.location.href='http://localhost:8080/browse-lost-items-result';
-    // setTimeout(displayResults, 1000);
+    setTimeout(displayResults, 1000);
 
     if ($('#searchTableID').val() == 'Lost Items') {
         queryLostItems();
@@ -17,30 +12,10 @@ $('#searchBtn').on('click', function() {
 });
 
 function displayResults() {
-    window.location.href='http://localhost:8080/browse-lost-items-result';
+    window.location.href='http://localhost:7070/browse-lost-items-result';
 }
 
-
-function queryLostItems() {
-    console.log('Table type: ' + $('#searchTableID').val());
-    console.log('Category: ' + $('#searchCategoryID').val());
-    console.log('Color: ' + $('#searchColorID').val());
-    console.log('Size: ' + $('#searchSizeID').val());
-    console.log('Subcategory ' + $('#categorySelect').val());
-
-    // var categoryVar = $('#searchCategoryID').val()
-    // var colorVar =  $('#searchColorID').val()
-    // var sizeVar = $('#searchSizeID').val()
-    // var subCategoryVar = $('#categorySelect').val()
-
-    var searchObj = {
-        category: $('#searchCategoryID').val(),
-        subcategory: $('#categorySelect').val(),
-        color: $('#searchColorID').val(),
-        size: $('#searchSizeID').val()
-    }
-    console.log('searchObj: ' + JSON.stringify(searchObj));
-
+function getSwitchExp(searchObj) {
     var switchExpression = '0';
     for (var property in searchObj) {
         if (searchObj[property] == 'Choose...') {
@@ -48,140 +23,148 @@ function queryLostItems() {
         }
     }
     console.log('Switch Expression: ' + switchExpression);
-
-    switch(switchExpression) {
-        case '0color': 
-        console.log('Missing: color only');
-        break;
-        case '0size': 
-        console.log('Missing: size only');
-        break;
-        case '0colorsize':
-        console.log('Missing: Color and Size');
-        break;
-        case '0categorysubcategory':
-        console.log('Missing: Category and Subcategory');
-        break;
-        case '0categorycolor':
-        console.log('Missing: Categories, subcategories, color');
-        break;
-        case '0categorysubcategorycolor':
-        console.log('Missing: Categories, subcategories, color');
-        break;
-        case '0categorysize':
-        console.log('Missing: Categories, subcategories, color');
-        break;
-        case '0categorysubcategorysize':
-        console.log('Missing: Categories, subcategories, size');
-        break;
-        case '0categorysubcategorycolorsize':
-        console.log('All inputs are valid');
-        break;
-        case '0categorycolorsize':
-        console.log('All inputs are valid');
-        break;
-    }
-
-
-//     if (categoryVar == null && subCategoryVar == null && colorVar !== 'Choose...'  && sizeVar !== 'Choose...'){
-//         console.log('Missing: Category and Subcategory only')
-//         searchData = {
-//             color: $('#searchColorID').val(),
-//             size: $('#searchSizeID').val(),
-//             // query: {
-//                 // color: req.query.color, 
-//                 // size: req.query.size
-//             // } 
-//         }
-//     } else if (colorVar== 'Choose...' && categoryVar !== null  && sizeVar !== 'Choose...') {
-//         console.log('Missing: Color only');
-//         searchData = {
-//             category: $('#searchCategoryID').val(),
-//             size: $('#searchSizeID').val(),
-//             // query: {
-//             //     category: req.query.category, 
-//             //     size: req.query.size
-//             // } 
-//         }
-// } else if (sizeVar == 'Choose...' && categoryVar !== null  && colorVar !== 'Choose...') {
-//     console.log('Missing: Size only');
-//     searchData = {
-//         category: $('#searchCategoryID').val(),
-//         color: $('#searchColorID').val(),
-//         // query: {
-//         //     category: req.query.category, 
-//         //     color: req.query.color
-//         // } 
-//     }
-// } else if (categoryVar == null && colorVar == 'Choose...' && sizeVar !== 'Choose...') {
-//     console.log('Missing: Category and Color');
-//     searchData = {
-//         size: $('#searchSizeID').val()
-//         // query: {
-//         //     sizeVar: req.query.size
-//         // } 
-//     }
-// } else if (categoryVar == null && sizeVar == 'Choose...' && colorVar !== 'Choose...') {
-//     console.log('Missing: Category and Size');
-//     searchData = {
-//         color: $('#searchColorID').val(),
-//         // query: {
-//         //     colorVar: req.query.color
-//         // } 
-//     }
-// } else if (categoryVar !== null && sizeVar == 'Choose...' && colorVar == 'Choose...') {
-//     console.log('Missing: Color and Size');
-//     searchData = {
-//         category: $('#searchCategoryID').val()
-//         // query: {
-//         //     category: req.query.category
-//         // } 
-//     }
-// } else if (categoryVar !== null && sizeVar !== 'Choose...' && colorVar !== 'Choose...') {
-//     console.log('All three inputs are valid');
-//     searchData = {
-//         category: $('#searchCategoryID').val(),
-//         color: $('#searchColorID').val(),
-//         size: $('#searchSizeID').val()
-//         // query: {
-//         //     category: req.query.category, 
-//         //     color: req.query.color,
-//         //     size: req.query.size
-//         // } 
-//     }
-// }
-
-    console.log("This is searchData: " + JSON.stringify(searchData));
-    
-    // $.ajax("/browse-lost-items", {
-    //     type: 'GET',
-    //     data: searchData
-    // }).then(function(data) {
-    // });
+    getSearchValues(switchExpression);
 };
 
-function queryFoundItems() {
-    console.log($('#searchTableID').val());
-    console.log($('#searchCategoryID').val());
-    console.log($('#searchColorID').val());
-    console.log($('#searchSizeID').val());
-
-    var searchData = {
-        category: $('#searchCategoryID').val(),
-        color: $('#searchColorID').val(),
-        size: $('#searchSizeID').val()
+    function getSearchValues(switchExpression) {
+        switch (switchExpression) {
+            case '0color':
+                console.log('Missing: color only');
+                var searchData = {
+                    category: $('#searchCategoryID').val(),
+                    subcategory: $('#categorySelect').val(),
+                    size: $('#searchSizeID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0size':
+                console.log('Missing: size only');
+                var searchData = {
+                    category: $('#searchCategoryID').val(),
+                    subcategory: $('#categorySelect').val(),
+                    color: $('#searchColorID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0colorsize':
+                console.log('Missing: Color and Size');
+                var searchData = {
+                    category: $('#searchCategoryID').val(),
+                    subcategory: $('#categorySelect').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorysubcategory':
+                console.log('Missing: Category and Subcategory');
+                var searchData = {
+                    color: $('#searchColorID').val(),
+                    size: $('#searchSizeID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorycolor':
+                console.log('Missing: Categories, subcategories, color');
+                var searchData = {
+                    size: $('#searchSizeID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorysubcategorycolor':
+                console.log('Missing: Categories, subcategories, color');
+                var searchData = {
+                    size: $('#searchSizeID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorysize':
+                console.log('Missing: Categories, subcategories, size');
+                var searchData = {
+                    color: $('#searchColorID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorysubcategorysize':
+                console.log('Missing: Categories, subcategories, size');
+                var searchData = {
+                    color: $('#searchColorID').val(),
+                    claimed: 0
+                };
+                break;
+            case '0categorysubcategorycolorsize':
+                console.log('No user inputs. All data will appear.');
+                var searchData = {
+                    claimed: 0
+                };
+                break;
+            case '0categorycolorsize':
+                console.log('No user inputs. All data will appear');
+                var searchData = {
+                    claimed: 0
+                };
+                break;
+            case '0':
+                console.log('All inputs are valid');
+                var searchData = {
+                    category: $('#searchCategoryID').val(),
+                    subcategory: $('#categorySelect').val(),
+                    color: $('#searchColorID').val(),
+                    size: $('#searchSizeID').val(),
+                    claimed: 0
+                };
+                break;
+        };
+        sendAjaxCall(searchData);
     };
 
-    $.ajax("/browse-found-items", {
-        type: 'GET',
-        data: searchData
-    }).then(function() {
-        
-        // console.log("This is data: " + data);
-        // $('#hbsContainer').append('browse-results', {lostItems: data});
-        // console.log("Querying Lost Items...")
-    });
-};
+    function sendAjaxCall(searchData) {
+        console.log("This is searchData: " + JSON.stringify(searchData));
+        $.ajax("/browse-lost-items", {
+            type: 'GET',
+            data: searchData
+        }).then(function(data) {
+        });
+    };
+
+    function queryLostItems() {
+        // Console.logged the values of the drop-downs to test
+        console.log('Table type: ' + $('#searchTableID').val());
+        console.log('Category: ' + $('#searchCategoryID').val());
+        console.log('Color: ' + $('#searchColorID').val());
+        console.log('Size: ' + $('#searchSizeID').val());
+        console.log('Subcategory ' + $('#categorySelect').val());
+
+        var searchObj = {
+            category: $('#searchCategoryID').val(),
+            subcategory: $('#categorySelect').val(),
+            color: $('#searchColorID').val(),
+            size: $('#searchSizeID').val()
+        }
+        console.log('The searchObj: ' + JSON.stringify(searchObj));
+        getSwitchExp(searchObj);
+    };
+
+    function queryFoundItems() {
+        console.log($('#searchTableID').val());
+        console.log($('#searchCategoryID').val());
+        console.log($('#searchColorID').val());
+        console.log($('#searchSizeID').val());
+
+        var searchData = {
+            category: $('#searchCategoryID').val(),
+            color: $('#searchColorID').val(),
+            size: $('#searchSizeID').val()
+        };
+
+        $.ajax("/browse-found-items", {
+            type: 'GET',
+            data: searchData
+        }).then(function () {
+
+            // console.log("This is data: " + data);
+            // $('#hbsContainer').append('browse-results', {lostItems: data});
+            // console.log("Querying Lost Items...")
+        });
+    };
 
 
 
--- routes/lostfound-api-routes.js
+++ routes/lostfound-api-routes.js
@@ -20,87 +20,29 @@ router.get('/lost', function (req, res) {
   })
 });
 
-// router.get('/browse-lost', function (req, res) {
-//   db.Lost.findAll({include:db.User}).then(function(data){
-//     res.render('browse',data)
-//   })
-// });
-
-// router.get('/api/find-lost-items', function(req, res) {
-//   db.Lost.findAll({
-//     where: {color: req.body.color}
-// }).then(function(data) {
-//   console.log(data);
-//   res.render('browse', {lostItems: data});
-// });
+router.get('/browse-lost-items-result', function(req, res) {
+  res.render('browse-results', {lostItems: globalData})
+});
 
 router.get('/browse-lost-items', function (req, res) {
-  console.log('The request: ' + JSON.stringify(req.query));
-  var query;
-  if (req.query.category && req.query.color && req.query.size == undefined) {
-    query = {
-      category: req.query.category,
-      color: req.query.color
-    }
-
-  } else if (req.query.color && req.query.size && req.query.category == undefined) {
-    query = {
-      color: req.query.color,
-      size: req.query.size
-    }
-  } else if (req.query.size && req.query.category && req.query.color == undefined) {
-    query = {
-      category: req.query.category,
-      size: req.query.size
-    }
-  } else if (req.query.size && req.query.category == undefined && req.query.color == undefined) {
-    query = {
-      size: req.query.size
-    }
-  } else if (req.query.color && req.query.category == undefined && req.query.size == undefined) {
-    query = {
-      color: req.query.color
-    }
-  } else if (req.query.category && req.query.color == undefined && req.query.size == undefined) {
-    query = {
-        category: req.query.category
-    }
-  } else if (req.query.category && req.query.color && req.query.size) {
-    query = {
-            category: req.query.category, 
-            color: req.query.color,
-            size: req.query.size
-    }
-  }
-
-    console.log('The query: ' + JSON.stringify(query));
+  console.log('The request/req.query: ' + JSON.stringify(req.query));
     db.Lost.findAll({
-      where: query
+      where: req.query
     }).then(function (data) {
       globalData = data;
-      
       console.log(data);
       console.log('Querying the lost items now...');
     });
   });
 
-router.get('/browse-lost-items-result', function(req, res) {
-  res.render('browse-results', {lostItems: globalData})
-});
-
-
 router.get('/browse-found-items', function (req, res) {
-  console.log(req.query);
-  db.Found.findAll({
-    where: {
-      // category: 'Electronics',
-      // // color: null,
-      // // size: null
-    }
+  console.log('The request/req.query: ' + JSON.stringify(req.query));
+  db.Lost.findAll({
+    where: req.query
   }).then(function (data) {
+    globalData = data;
     console.log(data);
-    console.log('Querying the lost items now...')
-    res.render('browse', { foundItems: data });
+    console.log('Querying the found items now...');
   });
 });
 
